9e37f45afce27804d01317d2dbf6f9082133d1a0
Pavel-Baturin <pavel.baturin.92@gmail.com>
2022-02-21 18:45:56 +0200

    Переводит решение ДЗ feedback на хуки

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 import Notification from './components/Notification/Notification';
 import s from './App.module.css';
 
-class App extends Component {
-  state = {
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbacks = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  handleFeedback = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  const totalFeedback = good + neutral + bad;
+  const goodPercentage = Math.round((good / totalFeedback) * 100);
+  const keys = Object.keys(feedbacks);
+
+  const handleFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  render() {
-    const keys = Object.keys(this.state);
-    const totalFeedback = Object.values(this.state).reduce((total, number) => {
-      return total + number;
-    }, 0);
-    const goodPercentage = Math.round((this.state.good / totalFeedback) * 100);
-    return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions options={keys} onLeaveFeedback={this.handleFeedback} />
-        <h2 className={s.title}>Statistics</h2>
-        {totalFeedback > 0 ? (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={totalFeedback}
-            positivePercentage={goodPercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </Section>
-    );
-  }
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions options={keys} onLeaveFeedback={handleFeedback} />
+      <h2 className={s.title}>Statistics</h2>
+      {totalFeedback > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={goodPercentage}
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </Section>
+  );
 }
 
 export default App;
